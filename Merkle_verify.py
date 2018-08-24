bcc0f05d86c5f032d952a639f4aaa7e1beb82f5a
ForGG <zhxyhhh@hotmail.com>
2018-08-24 16:12:40 -0400

    Another try for git

--- Merkle_verify.py
+++ Merkle_verify.py
@@ -10,7 +10,7 @@ mt.make_tree()
 
 print mt.get_leaf_count();
 
-print "root:", mt.get_merkle_root()  # root: '765f15d171871b00034ee55e48ffdf76afbc44ed0bcff5c82f31351d333c2ed1'
+print "root:", mt.get_merkle_root()  # root: '777765f15d171871b00034ee55e48ffdf76afbc44ed0bcff5c82f31351d333c2ed1'
 
 print mt.get_proof(1)  # [{left: '2da7240f6c88536be72abe9f04e454c6478ee29709fc3729ddfb942f804fbf08'},
                        #  {right: 'ef7797e13d3a75526946a3bcf00daec9fc9c9c4d51ddc7cc5df888f74dd434d1'}] 
